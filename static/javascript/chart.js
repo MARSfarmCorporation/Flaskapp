62b67c60513455cebe5195b58db006bec9748a2b
Ubuntu <ubuntu@ip-172-31-22-90.us-east-2.compute.internal>
2020-07-30 21:01:41 +0000

    Update 7/30/20

--- static/javascript/chart.js
+++ static/javascript/chart.js
@@ -18,6 +18,10 @@ function charting(email, duration){
         }
     }
 
+    for(var i=0; i< value.length; i++){
+      value[i] = parseFloat(value[i])
+    }
+
     if (duration == "168"){
         for(var i=0; i < time.length; i++){
             var res = time[i].split("T", 2)
@@ -39,8 +43,8 @@ function charting(email, duration){
             if(day == 0){
               break;
             }
+            value_7[0] = value_7[0] / count;
             value_7.unshift(value[i - 1]);
-            value_7[1] = value_7[1] / count;
             count = 1;
             date_7.unshift(time[i - 1]);
             day-=1;
@@ -50,16 +54,64 @@ function charting(email, duration){
         time = date_7;
         value = value_7;
     }
+
+    var date_all = [];
+    var value_all = [];
+    var max = [];
+    var min = [];
+    var count = 1;
+    if (duration == "100000") {
+      for(var i=0; i < time.length; i++){
+        var res = time[i].split("T", 2)
+        time[i] = res[0]
+      }
+      date_all.push(time[time.length-1]);
+      value_all.push(value[time.length-1]);
+      max.push(value[time.length-1]);
+      min.push(value[time.length-1]);
+      
+      for(var i = time.length - 1; i > 0; i--){
+        if(time[i] == time[i - 1]){
+          value_all[0] += value[i - 1];
+          count += 1;
+          if (value[i - 1] > max[0]){
+            max[0] = value[i - 1];
+          }
+          if (value[i - 1] < min[0]){
+            min[0] = value[i - 1];
+          }
+        }
+        else{
+          value_all[0] = value_all[0] / count;
+          value_all.unshift(value[i - 1]);
+          min.unshift(value[i - 1]);
+          max.unshift(value[i - 1]);
+          count = 1;
+
+          date_all.unshift(time[i - 1]);
+        }
+      }
+      value_all[0] = value_all[0] / count;
+      value = value_all;
+      time = date_all;
+    }
+
     var ctx = document.getElementById('myChart').getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: time,
             datasets: [{
-            label: 'Last 24 Hours',
             data: value,
             backgroundColor: "rgba(153,255,51,0.4)"
-            }]
+            },
+            {
+              data: min
+            },
+            {
+              data: max
+            }
+          ]
         }
     });
 }

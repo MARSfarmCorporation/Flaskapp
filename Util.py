af76389158902b6b2d354560df3a0b3a7c348aa7
Ubuntu <ubuntu@ip-172-31-30-110.us-east-2.compute.internal>
2020-08-17 18:52:49 +0000

    Add Comment 8/17/20

--- Util.py
+++ Util.py
@@ -1,5 +1,17 @@
+'''
+Util.py is a collection of utility functions that 
+will be used repeatedlly but have no dependencies
+on third-party library.
+
+Last Modified: 8/17/20
+'''
 import string
 
+'''
+This function take the encoded email in the url
+and decode it by pushing each character to the 
+previous one in ASCII encoding, except for "."
+'''
 def decode_secret(secret):
     
     result = ""
